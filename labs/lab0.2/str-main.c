841ff0127ba015978919eaebd919c03cb3927c38
Kevin Oswaldo Cabrera Navarro <kevintroko@gmail.com>
2019-02-03 22:15:45 -0600

    Hardcoded version of lab 2, ex:3

--- labs/lab0.2/str-main.c
+++ labs/lab0.2/str-main.c
@@ -2,15 +2,24 @@
 #include <stdio.h>
 
 int strlen2(char *str);
-
-int main()
+char* stradd2(char *origin, char *addition);
+char* strfind(char *origin, char *substr);
+ 
+int main(int argc, char *argv[])
 {
-    printf("Initial Lenght      : %d\n", strlen2("123456"));
+    if(argc > 3){
+        printf("Initial Lenght      : %d\n", strlen2(argv[1]));
+        // printf("New String          : %s\n", stradd2(argv[1], argv[2]));
+        printf("SubString was found : %s\n", strfind("hello", "hello"));
+    } else {
+        printf("incorrrect amount of arguments");
+    }
     return 0;
 }
 
 /*
 ./str-final.o originalVeryLongString Addition Add
+Initial Lenght      : 22
 New String          : originalVeryLongStringAddition
 SubString was found : yes
 */
--- labs/lab0.2/strlib.c
+++ labs/lab0.2/strlib.c
@@ -1,15 +1,25 @@
+#include <string.h>
 #include <stdio.h>
 
 int counter = 0;
+char *pch;
 
 int strlen2(char *str) {
     for (int i = 0 ; str[i]!='\0'; i++)
-    {
         counter++;
-    }
-    
-    printf("Counter is %d", counter);
     return counter;
 }
 
+char* stradd2(char *origin, char *addition){
+    return origin;
+}
 
+char* strfind(char *origin, char *substr) {
+    pch = strstr(origin , substr);
+    // printf("origin : %s\n", origin);
+    // printf("substr : %s\n", substr);
+    // printf("pch    : %s\n", pch);
+
+    if(pch!=NULL) return "yes";
+    else return "no";
+}
