3b5e46564c4928ee55c09d5c9771b7d446f3da89
Kevin Oswaldo Cabrera Navarro <kevintroko@gmail.com>
2019-02-10 15:56:20 -0600

    Lab 1.1, only pdf missing

---
+++ labs/lab1.1/mycat.c
@@ -0,0 +1,25 @@
+#include <unistd.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{   
+    char data[1000];
+
+    // Return -1 on error
+    int file = open(argv[1] , O_RDONLY); 
+    if (file == -1)
+        printf("ERROR WHILE OPENING FILE\n");
+
+    int fd = read(file, data, 1000);
+
+    if (fd < 0) {
+        printf("Error!\n");
+        return -1;
+    }
+    
+    printf("%s\n", data);
+    
+    return 0;
+}
