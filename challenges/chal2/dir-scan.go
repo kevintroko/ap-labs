961d20bae39f673551028dac979c21882d2b987e
Kevin Oswaldo Cabrera Navarro <kevintroko@gmail.com>
2019-02-15 22:37:51 -0600

    PANIC

--- challenges/chal2/dir-scan.go
+++ challenges/chal2/dir-scan.go
@@ -2,20 +2,84 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"os"
+	"path/filepath"
 )
 
-// scanDir stands for the directory scanning implementation
-func scanDir(dir string) error {
+func pathWalk(root string) []string {
+	// Kevin O. Cabrera 
+	var files []string
+	err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
+		files = append(files, path)
+		return nil
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	return files
+}
+
+func scanDir(root string) error {
+	// Kevin O. Cabrera 
+	files := pathWalk(root)
+	dirsLen := 0
+	permissionDeniedFiles := 0
+	symlinks := 0
+	otherFiles := 0
+	for i, file := range files {
+		if i == 0 {
+			continue
+		}
+		_, err := os.Readlink(file)
+		if err == nil {
+			symlinks++
+			continue
+		}
+
+		_, err = ioutil.ReadFile(file)
+		if err != nil {
+			switch err {
+			case os.ErrPermission:
+				permissionDeniedFiles++
+				continue
+			}
+		}
+		file, err := os.Stat(file)
+		if err == nil {
+			switch mode := file.Mode(); {
+			case mode.IsDir():
+				dirsLen++
+				continue
+			}
+		}
+
+		otherFiles++
+	}
+
+	fmt.Println("Directory Scanner tool")
+	fmt.Println("----------------------------")
+	fmt.Printf("Path %s\n", root)
+	fmt.Println("----------------------------")
+	fmt.Printf("Directories %d\n", dirsLen)
+	fmt.Println("----------------------------")
+	fmt.Printf("permission Denied Files %d\n", permissionDeniedFiles)
+	fmt.Println("----------------------------")
+	fmt.Printf("Symbolic Links %d\n", symlinks)
+	fmt.Println("----------------------------")
+	fmt.Printf("Other files %d", otherFiles)
+
 	return nil
 }
 
 func main() {
-
+	// Kevin O. Cabrera 
 	if len(os.Args) < 2 {
-		fmt.Println("Usage: ./dir-scan <directory>")
+		fmt.Println("Input string is required")
 		os.Exit(1)
 	}
 
-	scanDir(os.Args[1])
+	input := os.Args[1]
+	fmt.Println(scanDir(input))
 }
--- challenges/chal2/dmesg-analyzer.c
+++ challenges/chal2/dmesg-analyzer.c
@@ -1,40 +1,102 @@
-#include <stdio.h>
-#include <unistd.h>
 #include <fcntl.h>
+#include <stdbool.h>
 #include <stdio.h>
-#include <sys/types.h>
+#include <stdlib.h>
+#include <string.h>
 #include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
 
+#define _GNU_SOURCE
 #define REPORT_FILE "report.txt"
 
+// DIC
+#include <ctype.h>
+#define MAX_WORDS 100000
+#define DATA_SIZE 100000
+// DIC
+
 void analizeLog(char *logFile, char *report);
+char* formatToken(char *token);
 
 int main(int argc, char **argv) {
-
     if (argc < 2) {
 	printf("Usage:./dmesg-analizer.o logfile.txt\n");
 	return 1;
     }
-
     analizeLog(argv[1], REPORT_FILE);
-
     return 0;
 }
 
 void analizeLog(char *logFile, char *report) {
     printf("Generating Report from: [%s] log file\n", logFile);
+    // Variables
+    char data[DATA_SIZE];
+    FILE *fPtr;
+    fPtr = fopen("data/tokens.txt", "w");
+    if (fPtr == NULL) {
+        /* File not created hence exit */
+        printf("Unable to create file.\n");
+        exit(EXIT_FAILURE);
+    }
+    FILE *fp;
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t read;
+    // File open
+    fp = fopen("dmesg.txt", "r");
+    if (fp == NULL)
+        exit(EXIT_FAILURE);
 
-    // Implement your solution here.
-    char data[10000];
-    // Return -1 on error
-    int file = open(logFile, O_WRONLY);
+    while ((read = getline(&line, &len, fp)) != -1) {
+        char *s = line;
+        char str[10000];
+        strcpy(str, s);
+        char *token = strtok(str, "]");
+        token = formatToken(token);
+        fprintf(fPtr, "%s\n", token);
+        printf("%s: \n", token);
+        printf("%s\n", line);
+    }
+    fclose(fPtr);
+    fclose(fp);
+    if (line)
+        free(line);
+    exit(EXIT_SUCCESS);
 
-    if (file == -1)
-        printf("ERROR WHILE OPENING FILE\n");
-
-    int fd = read(file, data, 10000);
-
-    printf("%s is \n", data);
+    FILE *fptr;
+    char path[100];
+    int i2, len2, index2, isUnique2;
+    // List of distinct words
+    char words[MAX_WORDS][50];
+    char word[50];
+    // Count of distinct words
+    int count[MAX_WORDS];
+    /* Try to open file */
+    fptr = fopen("data/tokens.txt", "r");
+    /* Exit if file not opened successfully */
+    if (fptr == NULL) {
+        printf("Unable to open file.\n");
+        exit(EXIT_FAILURE);
+    }
+    // Initialize words count to 0
+    for (i2 = 0; i2 < MAX_WORDS; i2++)
+        count[i2] = 0;
 
     printf("Report is generated at: [%s]\n", report);
+}
+
+char* formatToken(char *token) {
+    token = strtok(NULL, " ");
+    if (token[strlen(token) - 1] == ':' || token[strlen(token) - 1] == ']') {
+        int i = 0;
+        while (token[i] != '\0') {
+            i++;
+        }
+        token[i - 1] = '\0';
+    }
+    if (token[0] == '[' || token[0] == '\t') {
+        token++;
+    }
+    return token;
 }
