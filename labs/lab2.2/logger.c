3832ab399be8edca066ffc371edbb3e90201d089
Kevin Oswaldo Cabrera Navarro <kevintroko@gmail.com>
2019-03-03 13:07:27 -0600

    Just colors missing

--- labs/lab2.2/logger.c
+++ labs/lab2.2/logger.c
@@ -9,11 +9,6 @@
 void openlog(const char *ident, int log_options, int facility);
 void syslog(int priority, const char *format, ...);
 
-int infof(const char *format, ...);
-int warnf(const char *format, ...);
-int errorf(const char *format, ...);
-int panicf(const char *format, ...);
-
  int infof(const char *format, ...) {
     va_list list;
     va_start(list, format);
@@ -51,13 +46,5 @@ int panicf(const char *format, ...);
     openlog(arg, (LOG_CONS|LOG_PERROR|LOG_PID), LOG_USER);
     syslog(LOG_EMERG, "%s", format);
     closelog();
-    return 0;
-}
-
-int main(int argc, char *argv[]) {
-    infof("info message", argv[0]);
-    warnf("warning message", argv[0]);
-    panicf("panic message", argv[0]);
-    errorf("error message", argv[0]);
     return 0;
 }
