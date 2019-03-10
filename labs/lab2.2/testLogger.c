1e8dc91e58084e21162ee20825b0918b604bdb75
Kevin Oswaldo Cabrera Navarro <kevintroko@gmail.com>
2019-03-10 14:50:22 -0600

    Working version

--- labs/lab2.2/testLogger.c
+++ labs/lab2.2/testLogger.c
@@ -1,9 +1,5 @@
 #include <stdio.h>
-
-int infof(const char *format, ...);
-int warnf(const char *format, ...);
-int errorf(const char *format, ...);
-int panicf(const char *format, ...);
+#include "logger.h"
 
 int main(int argc, char *argv[]) {
     infof("info message", argv[0]);
@@ -12,30 +8,3 @@ int main(int argc, char *argv[]) {
     errorf("error message", argv[0]);
     return 0;
 }
-
-/*
-#include <stdio.h>
-#include "logger.h"
-
-int main(){
-
-    // default logging
-    infof("INFO Message %d", 1);
-    warnf("WARN Message %d", 2);
-    errorf("ERROR Message %d", 2);
-
-    // stdout logging
-    initLogger("stdout");
-    infof("INFO Message %d", 1);
-    warnf("WARN Message %d", 2);
-    errorf("ERROR Message %d", 2);
-
-    // syslog logging
-    initLogger("syslog");
-    infof("INFO Message %d", 1);
-    warnf("WARN Message %d", 2);
-    errorf("ERROR Message %d", 2);
-
-    return 0;
-}
-*/
