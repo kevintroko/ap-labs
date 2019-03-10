0407454c68f9aea85c2dee76d44db18c0476e5e7
Kevin Oswaldo Cabrera Navarro <kevintroko@gmail.com>
2019-03-10 14:49:50 -0600

    Working version

--- labs/lab2.4/testLogger.c
+++ labs/lab2.4/testLogger.c
@@ -1,24 +1,29 @@
 #include <stdio.h>
 #include "logger.h"
 
-int main(){
-
-    // default logging
-    infof("INFO Message %d", 1);
-    warnf("WARN Message %d", 2);
-    errorf("ERROR Message %d", 2);
-
-    // stdout logging
-    initLogger("stdout");
-    infof("INFO Message %d", 1);
-    warnf("WARN Message %d", 2);
-    errorf("ERROR Message %d", 2);
-
-    // syslog logging
-    initLogger("syslog");
-    infof("INFO Message %d", 1);
-    warnf("WARN Message %d", 2);
-    errorf("ERROR Message %d", 2);
-
+int main(int argc, char *argv[]) {
+    infof("info message", argv[0]);
+    warnf("warning message", argv[0]);
+    panicf("panic message", argv[0]);
+    errorf("error message", argv[0]);
     return 0;
+
+    // // default logging
+    // infof("INFO Message %d", 1);
+    // warnf("WARN Message %d", 2);
+    // errorf("ERROR Message %d", 2);
+
+    // // stdout logging
+    // // initLogger("stdout");
+    // infof("INFO Message %d", 1);
+    // warnf("WARN Message %d", 2);
+    // errorf("ERROR Message %d", 2);
+
+    // // syslog logging
+    // // initLogger("syslog");
+    // infof("INFO Message %d", 1);
+    // warnf("WARN Message %d", 2);
+    // errorf("ERROR Message %d", 2);
+
+    // return 0;
 }
