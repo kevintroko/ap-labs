bf928b44f2f695dfaaa559482235d05bbab3131e
Kevin Oswaldo Cabrera Navarro <kevintroko@gmail.com>
2019-03-26 19:01:21 -0600

    Challenge 3

--- labs/lab3.2/base64.c
+++ labs/lab3.2/base64.c
@@ -9,16 +9,12 @@ REFERENCES
 https://en.wikibooks.org/wiki/Algorithm_Implementation/Miscellaneous/Base64
 */
 
-char b64[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
-
+char base64chars[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
 float progress;
 
-
-
-static void showProgress(int sig)
-{
-    infof("Progress: %.0f%c\n", progress, 37);
-}
+static void showProgress(int sig);
+void base64_encode(char *clrstr, char *b64dst);
+void decodeblock(unsigned char in[], char *clrstr);
 
 void decodeblock(unsigned char in[], char *clrstr)
 {
@@ -32,7 +28,7 @@ void decodeblock(unsigned char in[], char *clrstr)
 
 void b64_decode(char *b64src, char *clrdst)
 {
-    int c, phase, i;
+    int ch, phase, i;
     unsigned char in[4];
     char *p;
 
@@ -41,25 +37,22 @@ void b64_decode(char *b64src, char *clrdst)
     i = 0;
     while (b64src[i])
     {
-
         progress = ((float)i / (float)strlen(b64src)) * 100;
-
         if (signal(SIGINT, showProgress) == SIG_ERR)
             exit("Error in SIGINT");
-
         if (signal(SIGPWR, showProgress) == SIG_ERR)
             exit("Error in SIGPWR");
 
-        c = (int)b64src[i];
-        if (c == '=')
+        ch = (int)b64src[i];
+        if (ch == '=')
         {
             decodeblock(in, clrdst);
             break;
         }
-        p = strchr(b64, c);
+        p = strchr(base64chars, ch);
         if (p)
         {
-            in[phase] = p - b64;
+            in[phase] = p - base64chars;
             phase = (phase + 1) % 4;
             if (phase == 0)
             {
@@ -74,35 +67,31 @@ void b64_decode(char *b64src, char *clrdst)
 void encodeblock(unsigned char in[], char b64str[], int len)
 {
     unsigned char out[5];
-    out[0] = b64[in[0] >> 2];
-    out[1] = b64[((in[0] & 0x03) << 4) | ((in[1] & 0xf0) >> 4)];
-    out[2] = (unsigned char)(len > 1 ? b64[((in[1] & 0x0f) << 2) |
-                                           ((in[2] & 0xc0) >> 6)]
-                                     : '=');
-    out[3] = (unsigned char)(len > 2 ? b64[in[2] & 0x3f] : '=');
+    out[0] = base64chars[in[0] >> 2];
+    out[1] = base64chars[((in[0] & 0x03) << 4) | ((in[1] & 0xf0) >> 4)];
+    out[2] = (unsigned char)(len > 1 ? base64chars[((in[1] & 0x0f) << 2) |
+                                           ((in[2] & 0xc0) >> 6)] : '=');
+    out[3] = (unsigned char)(len > 2 ? base64chars[in[2] & 0x3f] : '=');
     out[4] = '\0';
     strncat(b64str, out, sizeof(out));
 }
 
-void b64_encode(char *clrstr, char *b64dst)
+void base64_encode(char *clrstr, char *b64dst)
 {
     unsigned char in[3];
     int i, len = 0;
     int j = 0;
-
     b64dst[0] = '\0';
     while (clrstr[j])
     {
-
         progress = ((float)j / (float)strlen(clrstr)) * 100;
-
         if (signal(SIGINT, showProgress) == SIG_ERR)
         {
-            exit("Error in SIGINT");
+            exit("Wrong input in SIGINT");
         }
 
         if (signal(SIGPWR, showProgress) == SIG_ERR)
-            exit("Error in SIGPWR");
+            exit("Wrong input in SIGPWR");
 
         len = 0;
         for (i = 0; i < 3; i++)
@@ -134,30 +123,22 @@ int main(int argc, char **argv)
 
     char *buffer = NULL;
     size_t size = 0;
-
     FILE *fp = fopen(argv[2], "r");
-
     fseek(fp, 0, SEEK_END);
     size = ftell(fp);
-
     rewind(fp);
-
     buffer = malloc((size + 1) * sizeof(*buffer));
-
     fread(buffer, size, 1, fp);
-
     buffer[size] = '\0';
-
     char *result = (char *)malloc(size * 10);
 
     if (strcmp(argv[1], "--encode") == 0)
     {
-        b64_encode(buffer, result);
+        base64_encode(buffer, result);
         FILE *f = fopen("encoded.txt", "wb");
         fprintf(f, "%s\n", result);
         fclose(f);
-    }
-    else if (strcmp(argv[1], "--decode") == 0)
+    } else if (strcmp(argv[1], "--decode") == 0)
     {
         b64_decode(buffer, result);
         FILE *f = fopen("decoded.txt", "wb");
@@ -169,6 +150,10 @@ int main(int argc, char **argv)
         printf("Invalid flag, try --encode or --decode.\n");
         return -1;
     }
-
     return 0;
+}
+
+static void showProgress(int sig)
+{
+    infof("Progress= %.0f%c\n", progress, 37);
 }
