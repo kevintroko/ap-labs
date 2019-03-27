5a75a57510f92636418c689833d43c4b29df780f
Kevin Oswaldo Cabrera Navarro <kevintroko@gmail.com>
2019-03-26 21:30:45 -0600

    Lab 3.2

--- labs/lab3.2/base64.c
+++ labs/lab3.2/base64.c
@@ -3,157 +3,271 @@
 #include <string.h>
 #include <stdlib.h>
 #include <signal.h>
+#include <inttypes.h>
+#include <string.h>
+#include <inttypes.h>
+#include <string.h>
+#include "logger.h"
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <signal.h>
+
+#define ENCODED_FILE "encoded.txt"
+#define DECODED_FILE "decoded.txt"
+#define WHITESPACE 64
+#define EQUALS 65
+#define INVALID 66
 
 /*
 REFERENCES 
 https://en.wikibooks.org/wiki/Algorithm_Implementation/Miscellaneous/Base64
 */
 
-char base64chars[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
-float progress;
-
-static void showProgress(int sig);
-void base64_encode(char *clrstr, char *b64dst);
-void decodeblock(unsigned char in[], char *clrstr);
-
-void decodeblock(unsigned char in[], char *clrstr)
+// Implementation of encode and decode taken from https://en.wikibooks.org/wiki/Algorithm_Implementation/Miscellaneous/Base64
+int base64encode(const void *data_buf, size_t dataLength, char *result, size_t resultSize)
 {
-    unsigned char out[4];
-    out[0] = in[0] << 2 | in[1] >> 4;
-    out[1] = in[1] << 4 | in[2] >> 2;
-    out[2] = in[2] << 6 | in[3] >> 0;
-    out[3] = '\0';
-    strncat(clrstr, out, sizeof(out));
-}
+    const char base64chars[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+    const uint8_t *data = (const uint8_t *)data_buf;
+    size_t resultIndex = 0;
+    size_t x;
+    uint32_t n = 0;
+    int padCount = dataLength % 3;
+    uint8_t n0, n1, n2, n3;
 
-void b64_decode(char *b64src, char *clrdst)
-{
-    int ch, phase, i;
-    unsigned char in[4];
-    char *p;
-
-    clrdst[0] = '\0';
-    phase = 0;
-    i = 0;
-    while (b64src[i])
+    /* increment over the length of the string, three characters at a time */
+    for (x = 0; x < dataLength; x += 3)
     {
-        progress = ((float)i / (float)strlen(b64src)) * 100;
-        if (signal(SIGINT, showProgress) == SIG_ERR)
-            exit("Error in SIGINT");
-        if (signal(SIGPWR, showProgress) == SIG_ERR)
-            exit("Error in SIGPWR");
+        /* these three 8-bit (ASCII) characters become one 24-bit number */
+        n = ((uint32_t)data[x]) << 16; //parenthesis needed, compiler depending on flags can do the shifting before conversion to uint32_t, resulting to 0
 
-        ch = (int)b64src[i];
-        if (ch == '=')
+        if ((x + 1) < dataLength)
+            n += ((uint32_t)data[x + 1]) << 8; //parenthesis needed, compiler depending on flags can do the shifting before conversion to uint32_t, resulting to 0
+
+        if ((x + 2) < dataLength)
+            n += data[x + 2];
+
+        /* this 24-bit number gets separated into four 6-bit numbers */
+        n0 = (uint8_t)(n >> 18) & 63;
+        n1 = (uint8_t)(n >> 12) & 63;
+        n2 = (uint8_t)(n >> 6) & 63;
+        n3 = (uint8_t)n & 63;
+
+        if (resultIndex >= resultSize)
+            return 1; 
+        result[resultIndex++] = base64chars[n0];
+        if (resultIndex >= resultSize)
+            return 1; 
+        result[resultIndex++] = base64chars[n1];
+
+        if ((x + 1) < dataLength)
         {
-            decodeblock(in, clrdst);
-            break;
+            if (resultIndex >= resultSize)
+                return 1; 
+            result[resultIndex++] = base64chars[n2];
         }
-        p = strchr(base64chars, ch);
-        if (p)
+
+        if ((x + 2) < dataLength)
         {
-            in[phase] = p - base64chars;
-            phase = (phase + 1) % 4;
-            if (phase == 0)
-            {
-                decodeblock(in, clrdst);
-                in[0] = in[1] = in[2] = in[3] = 0;
-            }
+            if (resultIndex >= resultSize)
+                return 1;
+            result[resultIndex++] = base64chars[n3];
         }
-        i++;
     }
-}
 
-void encodeblock(unsigned char in[], char b64str[], int len)
-{
-    unsigned char out[5];
-    out[0] = base64chars[in[0] >> 2];
-    out[1] = base64chars[((in[0] & 0x03) << 4) | ((in[1] & 0xf0) >> 4)];
-    out[2] = (unsigned char)(len > 1 ? base64chars[((in[1] & 0x0f) << 2) |
-                                           ((in[2] & 0xc0) >> 6)] : '=');
-    out[3] = (unsigned char)(len > 2 ? base64chars[in[2] & 0x3f] : '=');
-    out[4] = '\0';
-    strncat(b64str, out, sizeof(out));
-}
-
-void base64_encode(char *clrstr, char *b64dst)
-{
-    unsigned char in[3];
-    int i, len = 0;
-    int j = 0;
-    b64dst[0] = '\0';
-    while (clrstr[j])
+    if (padCount > 0)
     {
-        progress = ((float)j / (float)strlen(clrstr)) * 100;
-        if (signal(SIGINT, showProgress) == SIG_ERR)
+        for (; padCount < 3; padCount++)
         {
-            exit("Wrong input in SIGINT");
+            if (resultIndex >= resultSize)
+                return 1; 
+            result[resultIndex++] = '=';
         }
+    }
+    if (resultIndex >= resultSize)
+        return 1; 
+    result[resultIndex] = 0;
+    return 0;
+}
 
-        if (signal(SIGPWR, showProgress) == SIG_ERR)
-            exit("Wrong input in SIGPWR");
+static const unsigned char d[] = {
+    66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 64, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66,
+    66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 62, 66, 66, 66, 63, 52, 53,
+    54, 55, 56, 57, 58, 59, 60, 61, 66, 66, 66, 65, 66, 66, 66, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
+    10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 66, 66, 66, 66, 66, 66, 26, 27, 28,
+    29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 66, 66,
+    66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66,
+    66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66,
+    66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66,
+    66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66,
+    66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66,
+    66, 66, 66, 66, 66, 66};
 
-        len = 0;
-        for (i = 0; i < 3; i++)
+int base64decode(char *in, size_t inLen, unsigned char *out, size_t *outLen)
+{
+    char *end = in + inLen;
+    char iter = 0;
+    uint32_t buf = 0;
+    size_t len = 0;
+
+    while (in < end)
+    {
+        unsigned char c = d[*in++];
+
+        switch (c)
         {
-            in[i] = (unsigned char)clrstr[j];
-            if (clrstr[j])
+        case WHITESPACE:
+            continue; 
+        case INVALID:
+            return 1; 
+        case EQUALS:  
+            in = end;
+            continue;
+        default:
+            buf = buf << 6 | c;
+            iter++; 
+            if (iter == 4)
             {
-                len++;
-                j++;
+                if ((len += 3) > *outLen)
+                    return 1;
+                *(out++) = (buf >> 16) & 255;
+                *(out++) = (buf >> 8) & 255;
+                *(out++) = buf & 255;
+                buf = 0;
+                iter = 0;
             }
-            else
-                in[i] = 0;
         }
-        if (len)
+    }
+
+    if (iter == 3)
+    {
+        if ((len += 2) > *outLen)
+            return 1;
+        *(out++) = (buf >> 10) & 255;
+        *(out++) = (buf >> 2) & 255;
+    }
+    else if (iter == 2)
+    {
+        if (++len > *outLen)
+            return 1; 
+        *(out++) = (buf >> 4) & 255;
+    }
+
+    *outLen = len; 
+    return 0;
+}
+struct stat fileInfo;
+long sizeOfFile;
+long numberOfChars;
+
+void getProgress()
+{
+    long percentage = (numberOfChars * 100) / sizeOfFile;
+    infof("Progress of encoding/decoding = %ld %%\n", percentage);
+}
+
+void encodeOrDecode(int option, char *file)
+{
+    int fileRead;
+    if ((fileRead = open(file, O_RDONLY)) < 0)
+    {
+        errorf("Can't open file: %s\n", file);
+        exit(EXIT_FAILURE);
+    }
+
+    stat(file, &fileInfo);
+    sizeOfFile = fileInfo.st_size;
+
+    if (option == 1)
+    {
+        int fileWrite;
+        char *readBuffer, *writeBuffer;
+
+        if ((fileWrite = open(ENCODED_FILE, O_CREAT | O_WRONLY, S_IRUSR | S_IWUSR)) < 0)
         {
-            encodeblock(in, b64dst, len);
+            errorf("Can't create encoded file\n");
+            exit(EXIT_FAILURE);
         }
+
+        readBuffer = (char *)malloc(2);
+        writeBuffer = (char *)malloc(3);
+
+        int readedBytes;
+        while ((readedBytes = read(fileRead, readBuffer, 2)) > 0)
+        {
+            base64encode(readBuffer, 2, writeBuffer, 3);
+            write(fileWrite, writeBuffer, 3);
+            numberOfChars += readedBytes;
+        }
+        close(fileRead);
+        close(fileWrite);
+    }
+    else if (option == 2)
+    {
+        int fileWrite;
+        char *readBuffer;
+        unsigned char *writeBuffer;
+        size_t *writeBufferSize;
+
+        if ((fileWrite = open(DECODED_FILE, O_CREAT | O_WRONLY, S_IRUSR | S_IWUSR)) < 0)
+        {
+            errorf("Can't create decoded file\n");
+            exit(EXIT_FAILURE);
+        }
+
+        readBuffer = (char *)malloc(3);
+        writeBuffer = (unsigned char *)malloc(2);
+        writeBufferSize = (size_t *)malloc(sizeof(size_t));
+        *writeBufferSize = 2;
+
+        int readedBytes;
+        while ((readedBytes = read(fileRead, readBuffer, 3)) > 0)
+        {
+            base64decode(readBuffer, 3, writeBuffer, writeBufferSize);
+            write(fileWrite, writeBuffer, *writeBufferSize);
+            numberOfChars += readedBytes;
+        }
+        close(fileRead);
+        close(fileWrite);
     }
 }
 
 int main(int argc, char **argv)
 {
-    // Place your magic here
     if (argc < 3)
     {
-        printf("Error, not enough arguments. Usage: ./base64 --encode|--decode <input-file>\n");
-        return -1;
+        errorf("Usage encode: ./base64 --encode file\n");
+        errorf("Usage decode: ./base64 --decode file\n");
+        exit(EXIT_FAILURE);
     }
 
-    char *buffer = NULL;
-    size_t size = 0;
-    FILE *fp = fopen(argv[2], "r");
-    fseek(fp, 0, SEEK_END);
-    size = ftell(fp);
-    rewind(fp);
-    buffer = malloc((size + 1) * sizeof(*buffer));
-    fread(buffer, size, 1, fp);
-    buffer[size] = '\0';
-    char *result = (char *)malloc(size * 10);
+    if (signal(SIGINT, getProgress) == SIG_ERR)
+    {
+        errorf("Error with signal\n");
+        exit(EXIT_FAILURE);
+    }
+    if (signal(SIGPWR, getProgress) == SIG_ERR)
+    {
+        errorf("Error with signal\n");
+        exit(EXIT_FAILURE);
+    }
 
     if (strcmp(argv[1], "--encode") == 0)
     {
-        base64_encode(buffer, result);
-        FILE *f = fopen("encoded.txt", "wb");
-        fprintf(f, "%s\n", result);
-        fclose(f);
-    } else if (strcmp(argv[1], "--decode") == 0)
+        encodeOrDecode(1, argv[2]);
+    }
+    else if (strcmp(argv[1], "--decode") == 0)
     {
-        b64_decode(buffer, result);
-        FILE *f = fopen("decoded.txt", "wb");
-        fprintf(f, "%s\n", result);
-        fclose(f);
+        encodeOrDecode(2, argv[2]);
     }
     else
     {
-        printf("Invalid flag, try --encode or --decode.\n");
-        return -1;
+        errorf("Usage encode: ./base64 --encode file\n");
+        errorf("Usage decode: ./base64 --decode file\n");
+        exit(EXIT_FAILURE);
     }
-    return 0;
-}
 
-static void showProgress(int sig)
-{
-    infof("Progress= %.0f%c\n", progress, 37);
+    return 0;
 }
