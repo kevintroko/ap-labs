9daf7b5928460a0947d114b3c41124d62233a027
Kevin Oswaldo Cabrera Navarro <kevintroko@gmail.com>
2019-03-29 19:47:39 -0600

    GO lang

--- challenges/chal3/chat/server.go
+++ challenges/chal3/chat/server.go
@@ -9,19 +9,37 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"log"
 	"net"
+	"strings"
+	"time"
 )
 
 //!+broadcaster
 type client chan<- string // an outgoing message channel
 
+// defines a user
+type user struct {
+	Username string
+	IP       string
+}
+
 var (
-	entering = make(chan client)
-	leaving  = make(chan client)
-	messages = make(chan string) // all incoming client messages
+	entering    = make(chan client)
+	leaving     = make(chan client)
+	messages    = make(chan string) // all incoming client messages
+	addrToUsers = make(map[string]*user)
+	addrToConn  = make(map[string]net.Conn)
+	host        string
+	port        int
 )
+
+func init() {
+	flag.StringVar(&host, "host", "localhost", "flag for setting host")
+	flag.IntVar(&port, "port", 8080, "flag for setting port")
+}
 
 func broadcaster() {
 	clients := make(map[client]bool) // all connected clients
@@ -52,8 +70,10 @@ func handleConn(conn net.Conn) {
 	go clientWriter(conn, ch)
 
 	who := conn.RemoteAddr().String()
-	ch <- "You are " + who
-	messages <- who + " has arrived"
+
+	ch <- fmt.Sprintf("irc-server > Welcome to the IRC Server\nirc-server > Your user [%s] is successfully logged", addrToUsers[who].Username)
+	fmt.Printf("irc-server > New connected user [%s]\n", addrToUsers[who].Username)
+	messages <- fmt.Sprintf("user %s", addrToUsers[who].Username) + " has arrived"
 	entering <- ch
 
 	input := bufio.NewScanner(conn)
@@ -63,13 +83,62 @@ func handleConn(conn net.Conn) {
 	// NOTE: ignoring potential errors from input.Err()
 
 	leaving <- ch
-	messages <- who + " has left"
+	fmt.Printf("[%s] left\n", addrToUsers[who].Username)
+	messages <- fmt.Sprintf("%s", addrToUsers[who].Username) + " has left"
+	delete(addrToUsers, who)
+	delete(addrToConn, who)
+
 	conn.Close()
 }
 
 func clientWriter(conn net.Conn, ch <-chan string) {
 	for msg := range ch {
 		fmt.Fprintln(conn, msg) // NOTE: ignoring network errors
+		if strings.Contains(msg, "/user ") {
+			resp := "irc-server > "
+			userMsg := strings.Split(msg, " ")
+			for _, user := range addrToUsers {
+				if user.Username == userMsg[1] {
+					resp += fmt.Sprintf("username: %s, IP: %s", user.Username, user.IP)
+					break
+				}
+			}
+			conn.Write([]byte(resp))
+
+			continue
+		}
+		if strings.Contains(msg, "/msg ") {
+			resp := "irc-server > "
+			var destConn net.Conn
+			userMsg := strings.Split(msg, " ")
+			for _, user := range addrToUsers {
+				if user.Username == userMsg[1] {
+					destConn = addrToConn[user.IP]
+					for i := 2; i < len(userMsg); i++ {
+						resp += userMsg[i] + " "
+					}
+					break
+				}
+			}
+			strings.Trim(resp, " ")
+			destConn.Write([]byte(resp))
+
+			continue
+		}
+		switch {
+		case msg == "/users":
+			resp := "irc-server > "
+			for _, user := range addrToUsers {
+				resp += user.Username
+			}
+			conn.Write([]byte(resp)) //ignoring error
+
+		case msg == "/time":
+			resp := "irc-server > " + time.Now().String()
+			conn.Write([]byte(resp)) //ignoring error
+		default:
+			fmt.Println("dumb user")
+		}
 	}
 }
 
@@ -77,10 +146,15 @@ func clientWriter(conn net.Conn, ch <-chan string) {
 
 //!+main
 func main() {
-	listener, err := net.Listen("tcp", "localhost:8000")
+	flag.Parse()
+	address := fmt.Sprintf("%s:%d", host, port)
+	listener, err := net.Listen("tcp", address)
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	fmt.Printf("irc-server > Simple IRC Server started at %s\n", address)
+	fmt.Println("irc-server > Ready for receiving new clients")
 
 	go broadcaster()
 	for {
@@ -89,8 +163,20 @@ func main() {
 			log.Print(err)
 			continue
 		}
+		// body, err := ioutil.ReadAll(conn)
+		// if err != nil {
+		// 	fmt.Println("error reading:", err.Error())
+		// }
+		// addr := conn.RemoteAddr().String()
+		// addrToUsers[addr] = &user{
+		// 	Username: string(body),
+		// 	IP:       addr,
+		// }
+		// addrToConn[addr] = conn
+
 		go handleConn(conn)
 	}
+
 }
 
 //!-main
