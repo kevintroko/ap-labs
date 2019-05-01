4816fca6b385d912b55dbd2a30ab1d2b8111c2ae
Kevin Oswaldo Cabrera Navarro <kevintroko@gmail.com>
2019-04-30 20:02:04 -0500

    pi_spmd_final added

--- labs/lab5.1/hello_par.c
+++ labs/lab5.1/hello_par.c
@@ -1,4 +1,21 @@
+#include <omp.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-int main() {
-    return 0;
+int main(int argc, char *argv[])
+{
+    int nthreads, tid;
+
+#pragma omp parallel private(nthreads, tid)
+    {
+        tid = omp_get_thread_num();
+        printf("Hello World from thread = %d\n", tid);
+
+        if (tid == 0)
+        {
+            nthreads = omp_get_num_threads();
+            printf("Number of threads = %d\n", nthreads);
+        }
+
+    }
 }
