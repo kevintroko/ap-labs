4e95c1b8cfb7bf0dbc185bd8869a43ee2ea7a678
Kevin Oswaldo Cabrera Navarro <kevintroko@gmail.com>
2019-05-10 21:08:26 -0500

    Create island (surrounded by sea).

--- challenges/island/island.c
+++ challenges/island/island.c
@@ -1,10 +1,53 @@
 #include <stdio.h>
 
-int island[10][10];
+// @Todo check if an struct is better
+#define SIZE 5
+#define TOP 0
+#define LOW 9
 
+// Main Function
 int main() {
-    island[0][0] = 1;
-    island[0][1] = 1;
-    printf("hello %d\n", island[0][0]);
-    return 1;
+    // Create Island
+    int island[SIZE][SIZE];
+
+    // Generate values for each island cell
+    for(int i = 0; i < SIZE; i++) {
+        for(int j = 0; j < SIZE; j++) {
+
+            // Make north sea
+            if(i==0) {
+                island[i][j] = 0;
+            } 
+
+            // Make west sea
+            else if(j==0) {
+                island[i][j] = 0;
+            } 
+
+            // Make east sea
+            else if(j==(SIZE-1)) {
+                island[i][j] = 0;
+            } 
+
+            // Make south sea
+            else if(i==(SIZE-1)) {
+                island[i][j] = 0;
+            } 
+
+            else {
+                island[i][j] = 2;
+            }
+        }
+    }
+    
+    printf("----------------\n");
+    for(int i = 0; i < SIZE; i++) {
+        for(int j = 0; j < SIZE; j++) {
+                printf("[%d]", island[i][j]);
+        }
+        printf("\n");
+    }
+    printf("----------------\n");
+
+    return 0;
 }
